8b24b109a13607897000894705861d0a0e8ee132
skoller <nskoller@gmail.com>
2016-07-15 21:12:29 -0500

    config assests changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # WAS SET TO FALSE, CHANGED TO GET HEROKU TO WORK
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 

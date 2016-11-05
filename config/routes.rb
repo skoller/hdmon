325f95931518d60bb524093c8b0f8defea8bc47e
skoller <nskoller@gmail.com>
2016-11-05 14:05:41 -0400

    changed match to via

--- config/routes.rb
+++ config/routes.rb
@@ -8,56 +8,56 @@ Hdmon::Application.routes.draw do
     end
   end
   
-  match 'physician_signup_part2/:id(.:format)' => 'physicians#signup_part2', :as => "physician_signup_part2"
-  match "home_page" => "application#home_page", :as => "home_page"
-  match "deactivate_pt_msg/:physician_id/:patient_id" => "physicians#deactivate_pt_message", :as => "deactivate_pt_message"
-  match "deactivate_ph_msg/:physician_id" => "physicians#deactivate_ph_message", :as => "deactivate_ph_message"
-  match "ph_deactivate" => "physicians#destroy_archive", :as => "destroy_archive"
-  match "pt_archive/:physician_id/:id" => "patients#archive", :as => "archive_pt"
-  match "physician/:physician_id/patient_archive" => "physicians#pt_archive_index", :as => "pt_archive_index"
-  match "code" => "sessions#new_patient_code_verification", :as => "patient_code"
-  match "new_patient" => "sessions#new_patient_start_code_entry", :as => "new_patient_start_code_entry"
-  match "new_patient_password_setup/:pt_id" => "sessions#new_patient_password_setup", :as => "new_patient_password_setup"
-  match "p_set/:pt_id" => "sessions#password_set", :as => "password_set"
+  via 'physician_signup_part2/:id(.:format)' => 'physicians#signup_part2', :as => "physician_signup_part2"
+  via "home_page" => "application#home_page", :as => "home_page"
+  via "deactivate_pt_msg/:physician_id/:patient_id" => "physicians#deactivate_pt_message", :as => "deactivate_pt_message"
+  via "deactivate_ph_msg/:physician_id" => "physicians#deactivate_ph_message", :as => "deactivate_ph_message"
+  via "ph_deactivate" => "physicians#destroy_archive", :as => "destroy_archive"
+  via "pt_archive/:physician_id/:id" => "patients#archive", :as => "archive_pt"
+  via "physician/:physician_id/patient_archive" => "physicians#pt_archive_index", :as => "pt_archive_index"
+  via "code" => "sessions#new_patient_code_verification", :as => "patient_code"
+  via "new_patient" => "sessions#new_patient_start_code_entry", :as => "new_patient_start_code_entry"
+  via "new_patient_password_setup/:pt_id" => "sessions#new_patient_password_setup", :as => "new_patient_password_setup"
+  via "p_set/:pt_id" => "sessions#password_set", :as => "password_set"
   
   
   
   # flash.now.alert=
-  match 'phone/sms_handler' => "phone#sms_handler", :as => "sms_handler"
-  match 'after_start_code_web_handler' => "patients#after_start_code_web_handler", :as => "after_start_code_web_handler"
+  via 'phone/sms_handler' => "phone#sms_handler", :as => "sms_handler"
+  via 'after_start_code_web_handler' => "patients#after_start_code_web_handler", :as => "after_start_code_web_handler"
   
-  match "physician_log_out" => "sessions#destroy_ph", :as => "log_out"
-  match "physician_log_in" => "sessions#new_physician_session", :as => "log_in"
-  match "physician_find" => "sessions#create_ph_session", :as => "matched"
-  match "physician_sign_up" => "physicians#new", :as => "sign_up"
-  match "physician_additional_info" => "physicians#physician_additional_info", :as => "physician_additional_info"
-  match "welcome_ph_instructions/:physician_id" => "physicians#welcome_ph_instructions", :as => "welcome_ph_instructions" 
-  match "unarch_pt/:id/:patient_id" => "physicians#unarchive_patient", :as => "unarchive_patient"
-  match "physician_account/:physician_id" => "physicians#physician_account", :as => "physician_account"
-  match "edit_physician_account/:physician_id" => "physicians#edit_physician_account", :as => "edit_physician_account"
-  match "physician_pswd_edit/:physician_id" => "physicians#ph_password_edit", :as => 'ph_password_edit'
-  match "physician_pswd_update/:physician_id" => "physicians#ph_password_update", :as => 'ph_password_update'
-  
-  
-  match "patient_log_out" => "sessions#destroy_pt", :as => "patient_log_out"
-  match "patient_log_in" => "sessions#new_patient_session", :as => "patient_log_in"
-  match "patient_find" => "sessions#create_pt_session", :as => "patient_matched"
-  match "patient_edit/:patient_id" => "patients#patient_edit_limited", :as => "pt_edit"
-  match "patient_show/:patient_id" => "patients#patient_show_limited", :as => "pt_show"
-  match "patient_update/:patient_id" => "patients#patient_update_limited", :as => "pt_update"
-  match "patient_welcome/:patient_id" => "patients#patient_welcome", :as => "patient_welcome"
+  via "physician_log_out" => "sessions#destroy_ph", :as => "log_out"
+  via "physician_log_in" => "sessions#new_physician_session", :as => "log_in"
+  via "physician_find" => "sessions#create_ph_session", :as => "viaed"
+  via "physician_sign_up" => "physicians#new", :as => "sign_up"
+  via "physician_additional_info" => "physicians#physician_additional_info", :as => "physician_additional_info"
+  via "welcome_ph_instructions/:physician_id" => "physicians#welcome_ph_instructions", :as => "welcome_ph_instructions" 
+  via "unarch_pt/:id/:patient_id" => "physicians#unarchive_patient", :as => "unarchive_patient"
+  via "physician_account/:physician_id" => "physicians#physician_account", :as => "physician_account"
+  via "edit_physician_account/:physician_id" => "physicians#edit_physician_account", :as => "edit_physician_account"
+  via "physician_pswd_edit/:physician_id" => "physicians#ph_password_edit", :as => 'ph_password_edit'
+  via "physician_pswd_update/:physician_id" => "physicians#ph_password_update", :as => 'ph_password_update'
   
   
-  match "admin_override_pt_password_edit/:patient_id" => "patients#admin_pt_password_edit", :as => "admin_pt_pass_edit"
-  match "admin_override_pt_password_update/:patient_id" => "patients#admin_pt_password_update", :as => "admin_pt_pass_update"
-  match "admin" => "admin#index", :as => "admin"
-  match "archived_physicians" => "admin#archived_physicians", :as => "archived_physicians"
-  match "archived_patients" => "admin#archived_patients", :as => "archived_patients"
-  match "active_patients" => "admin#active_patients", :as => "active_patients"
-  match "deact_and_arch_pt/:patient_id" => "admin#deactivate_and_archive_patient", :as => "deactivate_and_archive_patient"
-  match "react_and_unarch_pt/:patient_id" => "admin#reactivate_and_unarchive_patient", :as => "reactivate_and_unarchive_patient"
-  match "archive_a_physician/:physician_id" => "admin#archive_a_physician", :as => "archive_a_physician"
-  match "unarchive_a_physician/:physician_id" => "admin#unarchive_a_physician", :as => "unarchive_a_physician"
+  via "patient_log_out" => "sessions#destroy_pt", :as => "patient_log_out"
+  via "patient_log_in" => "sessions#new_patient_session", :as => "patient_log_in"
+  via "patient_find" => "sessions#create_pt_session", :as => "patient_viaed"
+  via "patient_edit/:patient_id" => "patients#patient_edit_limited", :as => "pt_edit"
+  via "patient_show/:patient_id" => "patients#patient_show_limited", :as => "pt_show"
+  via "patient_update/:patient_id" => "patients#patient_update_limited", :as => "pt_update"
+  via "patient_welcome/:patient_id" => "patients#patient_welcome", :as => "patient_welcome"
+  
+  
+  via "admin_override_pt_password_edit/:patient_id" => "patients#admin_pt_password_edit", :as => "admin_pt_pass_edit"
+  via "admin_override_pt_password_update/:patient_id" => "patients#admin_pt_password_update", :as => "admin_pt_pass_update"
+  via "admin" => "admin#index", :as => "admin"
+  via "archived_physicians" => "admin#archived_physicians", :as => "archived_physicians"
+  via "archived_patients" => "admin#archived_patients", :as => "archived_patients"
+  via "active_patients" => "admin#active_patients", :as => "active_patients"
+  via "deact_and_arch_pt/:patient_id" => "admin#deactivate_and_archive_patient", :as => "deactivate_and_archive_patient"
+  via "react_and_unarch_pt/:patient_id" => "admin#reactivate_and_unarchive_patient", :as => "reactivate_and_unarchive_patient"
+  via "archive_a_physician/:physician_id" => "admin#archive_a_physician", :as => "archive_a_physician"
+  via "unarchive_a_physician/:physician_id" => "admin#unarchive_a_physician", :as => "unarchive_a_physician"
   
 
     

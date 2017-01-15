4f12b9627a42bf8411aa2c7ded733b23d80563c5
skoller <nskoller@gmail.com>
2017-01-15 12:27:51 -0600

    try delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105192155) do
+ActiveRecord::Schema.define(version: 20170115061016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +21,21 @@ ActiveRecord::Schema.define(version: 20161105192155) do
     t.integer  "log_entry_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
   end
 
   create_table "log_entries", force: :cascade do |t|
---
+++ lib/Sms.rb
@@ -0,0 +1,25 @@
+class Sms
+  include Delayed::RecurringJob
+  run_every 1.minute
+  run_at '1:34pm'
+  timezone 'US/Pacific'
+  queue 'slow-jobs'
+  def perform
+    number_to_send_to = '3108090426'
+    account_sid = 'AC0e331b7fa11f13be73a32e5311a74969'
+    auth_token = 'e948aaf8caad373ae54918c175fd8786'
+    twilio_phone_number = "3104992061"
+
+    @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+    @twilio_client.account.messages.create(
+                 :from => "+1#{twilio_phone_number}",
+                 :to => number_to_send_to,
+                 :body => "Congratulations!"
+               )
+           @twilio_client.account.messages.create(
+                 :from => "+1#{twilio_phone_number}",
+                 :to => number_to_send_to,
+                 :body => "You have communicated with HdMon"
+               )
+  end
+end

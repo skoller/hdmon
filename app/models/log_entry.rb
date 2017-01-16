6c9b3412c704e57cbf0fc2692c7fa3dcdaf715c9
skoller <nskoller@gmail.com>
2017-01-15 20:43:23 -0600

    try recurring msg

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -2,6 +2,13 @@ class LogEntry < ApplicationRecord
   belongs_to :patient
   has_one :convo_handler
 
+  include Delayed::RecurringJob
+  run_every 1.minute
+  run_at '6:45pm'
+  timezone 'US/Pacific'
+  queue 'slow-jobs'
+
+
   def drop_it
     self.destroy
   end
@@ -12,7 +19,7 @@ class LogEntry < ApplicationRecord
       return (self.date.to_time.strftime('%a %b ')) + day
     end
   end
-  
+
   def year_web_display
     if self.date
       return self.date.to_time.strftime(' %Y')
@@ -25,5 +32,24 @@ class LogEntry < ApplicationRecord
       return (hour + self.date.to_time.strftime(':%M%p'))
     end
   end
-  
+
+  def perform
+    number_to_send_to = '3108090426'
+    account_sid = 'AC0e331b7fa11f13be73a32e5311a74969'
+    auth_token = 'e948aaf8caad373ae54918c175fd8786'
+    twilio_phone_number = "3104992061"
+
+    @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+    @twilio_client.account.messages.create(
+                 :from => "+1#{twilio_phone_number}",
+                 :to => number_to_send_to,
+                 :body => "Congratulations!"
+               )
+           @twilio_client.account.messages.create(
+                 :from => "+1#{twilio_phone_number}",
+                 :to => number_to_send_to,
+                 :body => "You have communicated with HdMon"
+               )
+  end
+
 end
